29a7628d8e5d2946fc65dd8d7cc42eb251485df1
Madhav Agarwal <madhav.agarwal@research.iiit.ac.in>
2021-04-20 01:32:34 +0530

    Add inception_small

---
+++ inception_small.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+
+class ConvModule(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(ConvModule, self).__init__()
+        
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = nn.ReLU()
+        
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
+        return x
+ 
+class InceptionModule(nn.Module):
+    
+    def __init__(self, in_channels, f_1x1, f_3x3):
+        super(InceptionModule, self).__init__()
+        
+        self.branch1 = nn.Sequential(
+            ConvModule(in_channels, f_1x1, kernel_size=1, stride=1, padding=0)
+        )
+        
+        self.branch2 = nn.Sequential(
+            ConvModule(in_channels, f_3x3, kernel_size=3, stride=1, padding=1)
+        )
+                
+    def forward(self, x):
+        branch1 = self.branch1(x)
+        branch2 = self.branch2(x)
+        return torch.cat([branch1, branch2], 1)
+
+
+class DownsampleModule(nn.Module):
+    def __init__(self, in_channels, f_3x3):
+        super(DownsampleModule, self).__init__()
+    
+        self.branch1 = nn.Sequential(ConvModule(in_channels, f_3x3, kernel_size=3, stride=2, padding=0))
+        self.branch2 = nn.MaxPool2d(3, stride=2, padding=1, ceil_mode=True)
+    
+    def forward(self, x):
+        branch1 = self.branch1(x)
+        branch2 = self.branch2(x)
+        return torch.cat([branch1, branch2], 1)
+   
+class InceptionSmall(nn.Module):
+    def __init__(self, num_classes = 10):
+        super(InceptionSmall, self).__init__()
+        
+        self.conv1 = ConvModule(in_channels =3,out_channels=96, kernel_size=3, stride=1, padding=0)
+        self.inception1 = InceptionModule(in_channels=96,f_1x1=32,f_3x3=32)
+        self.inception2 = InceptionModule(in_channels=64,f_1x1=32,f_3x3=48)
+        self.down1 = DownsampleModule(in_channels=80,f_3x3=80)
+        self.inception3 = InceptionModule(in_channels=160,f_1x1=112,f_3x3=48)
+        self.inception4 = InceptionModule(in_channels=160,f_1x1=96,f_3x3=64)
+        self.inception5 = InceptionModule(in_channels=160,f_1x1=80,f_3x3=80)
+        self.inception6 = InceptionModule(in_channels=160,f_1x1=48,f_3x3=96)   
+        self.down2 = DownsampleModule(in_channels=144,f_3x3=96)
+        self.inception7 = InceptionModule(in_channels=240,f_1x1=176,f_3x3=160)
+        self.inception8 = InceptionModule(in_channels=336,f_1x1=176,f_3x3=160)
+        self.meanpool = nn.AdaptiveAvgPool2d((7,7))
+        self.fc = nn.Linear(1024, num_classes)
+       
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.inception1(x)
+        x = self.inception2(x)
+        x = self.donw1(x)
+        x = self.inception3(x)
+        x = self.inception4(x)
+        x = self.inception5(x)
+        x = self.inception6(x)
+        x = self.donw2(x)
+        x = self.inception7(x)
+        x = self.inception8(x)
+        x = self.meanpool(x)
+        x = torch.flatten(x,1)
+        x = fc(x)
+        return x
+        return torch.cat([branch1, branch2], 1)
